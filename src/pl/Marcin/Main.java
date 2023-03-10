173b32155bf0ec0fd05114f9726526bbf8cc86c1
Marcin <gasiorek.mail@gmail.com>
2023-03-10 11:34:22 +0100

    relative read and absolute read in NIO

--- src/pl/Marcin/Main.java
+++ src/pl/Marcin/Main.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,16 +33,45 @@ try(FileOutputStream binFile = new FileOutputStream("data.dat");
     numBytes = binChannel.write(intBuffer);
     System.out.println("numBytes written was : " + numBytes);
 
+//    RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//    byte[] b = new byte[outputBytes.length];
+//    ra.read(b);
+//    System.out.println(new String(b));
+//
+//    long int1 = ra.readInt();
+//    long int2 = ra.readInt();
+//    System.out.println(int1);
+//    System.out.println(int2);
+//    reading with JAva.INO:
     RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+    FileChannel channel = ra.getChannel();
+    outputBytes[0] = 'a';
+    outputBytes[1]= 'b';
+    buffer.flip();
+    long numBytesRead = channel.read(buffer);
+    if(buffer.hasArray()){
+        System.out.println("byte buffer " + new String(buffer.array()));
+    }
+//    Absolute read
+    intBuffer.flip();
+    numBytesRead = channel.read(intBuffer);
+    System.out.println(intBuffer.getInt(0));
+    intBuffer.flip();
+    numBytesRead = channel.read(intBuffer);
+    System.out.println(intBuffer.getInt(0));
 
-    byte[] b = new byte[outputBytes.length];
-    ra.read(b);
-    System.out.println(new String(b));
-
-    long int1 = ra.readInt();
-    long int2 = ra.readInt();
-    System.out.println(int1);
-    System.out.println(int2);
+//    relative read
+//    intBuffer.flip();
+//    numBytesRead = channel.read(intBuffer);
+//    intBuffer.flip();
+//    System.out.println(intBuffer.getInt());
+//    intBuffer.flip();
+//    numBytesRead = channel.read(intBuffer);
+//    intBuffer.flip();
+//    System.out.println(intBuffer.getInt());
+    channel.close();
+    ra.close();
+//    System.out.println("outputbytes = " + new String(outputBytes));
 
 
 
