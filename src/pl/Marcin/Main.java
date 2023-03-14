1f911c867230cf20112fa58c6a3dcb4e0af07cc6
Marcin <gasiorek.mail@gmail.com>
2023-03-14 08:43:17 +0100

    Pipes

--- src/pl/Marcin/Main.java
+++ src/pl/Marcin/Main.java
@@ -67,47 +67,52 @@ try(FileOutputStream binFile = new FileOutputStream("data.dat");
     readBuffer.flip();
     System.out.println("int1 = " + readBuffer.getInt());
 
+    //copy data :
+    RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat","rw");
+    FileChannel copyChannel = copyFile.getChannel();
+    channel.position(0);
+//    long numTransferred = copyChannel.transferFrom(channel,0,channel.size());
+       long numTransferred = channel.transferTo(0,channel.size(),copyChannel);
+
+    System.out.println("numTransferred = " + numTransferred );
+
+    channel.close();
+    ra.close();
+    copyChannel.close();
+
+
+
 //    write data in a random fashion:
-    byte[]outputString = "Hello".getBytes();
-    long str1Pos = 0;
-    long newInt1Pos = outputString.length;
-    long newInt2Pos = newInt1Pos + Integer.BYTES;
-    byte[] outputString2 = "Nice to".getBytes();
-    long str2Pos = newInt2Pos + Integer.BYTES;
-    long newInt3Pos = str2Pos + outputString2.length;
-
-    ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-    intBuffer.putInt(245);
-    intBuffer.flip();
-    binChannel.position(newInt1Pos);
-    binChannel.write(intBuffer);
-
-    intBuffer.flip();
-    intBuffer.putInt(-98765);
-    intBuffer.flip();
-    binChannel.position(newInt2Pos);
-    binChannel.write(intBuffer);
-
-    intBuffer.flip();
-    intBuffer.putInt(1000);
-    intBuffer.flip();
-    binChannel.position(newInt3Pos);
-    binChannel.write(intBuffer);
-
-    binChannel.position(str1Pos);
-    binChannel.write(ByteBuffer.wrap(outputString));
-    binChannel.position(str2Pos);
-    binChannel.write(ByteBuffer.wrap(outputString2));
-
-
-
-
-
-
-
-
-
-
+//    byte[]outputString = "Hello".getBytes();
+//    long str1Pos = 0;
+//    long newInt1Pos = outputString.length;
+//    long newInt2Pos = newInt1Pos + Integer.BYTES;
+//    byte[] outputString2 = "Nice to".getBytes();
+//    long str2Pos = newInt2Pos + Integer.BYTES;
+//    long newInt3Pos = str2Pos + outputString2.length;
+//
+//    ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+//    intBuffer.putInt(245);
+//    intBuffer.flip();
+//    binChannel.position(newInt1Pos);
+//    binChannel.write(intBuffer);
+//
+//    intBuffer.flip();
+//    intBuffer.putInt(-98765);
+//    intBuffer.flip();
+//    binChannel.position(newInt2Pos);
+//    binChannel.write(intBuffer);
+//
+//    intBuffer.flip();
+//    intBuffer.putInt(1000);
+//    intBuffer.flip();
+//    binChannel.position(newInt3Pos);
+//    binChannel.write(intBuffer);
+//
+//    binChannel.position(str1Pos);
+//    binChannel.write(ByteBuffer.wrap(outputString));
+//    binChannel.position(str2Pos);
+//    binChannel.write(ByteBuffer.wrap(outputString2));
 
 
 //    ByteBuffer readBuffer = ByteBuffer.allocate(100);
@@ -123,16 +128,6 @@ try(FileOutputStream binFile = new FileOutputStream("data.dat");
 //    readBuffer.get(inputString2);
 //    System.out.println("InputString2 = " + new String(inputString2));
 //    System.out.println("int3 =" + readBuffer.getInt());
-
-
-
-
-
-
-
-
-
-
 
 //    ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
 //    buffer.put(outputBytes);
